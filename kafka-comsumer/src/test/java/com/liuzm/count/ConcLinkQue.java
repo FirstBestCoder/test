59d78915062fdcb7bb4be862a7f164b636ddfde6
liuzm <1256512817@qq.com>
2020-07-28 10:01:31 +0800

    2020-7-28第二次liuzm提交代码

--- kafka-comsumer/src/test/java/com/liuzm/count/ConcLinkQue.java
+++ kafka-comsumer/src/test/java/com/liuzm/count/ConcLinkQue.java
@@ -6,17 +6,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ConcLinkQue {
     public static void main(String[] args) {
-
-        ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<String>();
-        queue.offer("123");
-        queue.offer("234");
-        Object[] objects = queue.toArray();
-        System.out.println(objects[0] + ", " + objects[1]);
-
-        //将数据存储到指定数组
-        String[] s = new String[2];
-        queue.toArray(s);
-        System.out.println(Arrays.toString(s));
+        //TODO
 
     }
 }

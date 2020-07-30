4781466ecc874790fb5b87c0057f2a13e630fd23
liuzm <1256512817@qq.com>
2020-07-30 09:34:15 +0800

    2020-7-30修改代码ThreeClass

--- src/main/java/com/liuzm/test/ThreeClass.java
+++ src/main/java/com/liuzm/test/ThreeClass.java
@@ -3,6 +3,6 @@ package com.liuzm.test;
 public class ThreeClass {
     public static void main(String[] args) {
         //TODO
-        System.out.println("第三个测试文件");
+        //System.out.println("第三个测试文件");
     }
 }
